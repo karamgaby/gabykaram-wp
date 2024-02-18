cf9fa9426ad1da5a867c93cef84faeab55046e36
Gaby Karam <gaby@polarstork.com>
2024-02-19 00:57:57 +0200

    add theme initial files

---
+++ wp-content/themes/stage-zero/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gabykaram/block-theme',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bd22f71eeabfddb6b1dc47a93df764f21389b543',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gabykaram/block-theme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bd22f71eeabfddb6b1dc47a93df764f21389b543',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
