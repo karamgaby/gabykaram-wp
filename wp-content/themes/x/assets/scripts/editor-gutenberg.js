78192f0c7091a364ac02acd9525d334b37ad07d1
Gaby Karam <gaby@polarstork.com>
2024-02-26 04:26:42 +0200

    fixes

--- wp-content/themes/x/assets/scripts/editor-gutenberg.js
+++ wp-content/themes/x/assets/scripts/editor-gutenberg.js
@@ -14,29 +14,29 @@ wp.hooks.addFilter(
   (size, attachment_id, post_id) => 'medium'
 );
 
-/**
- * Force default block classnames (ie. 'wp-block-list')
- */
-wp.hooks.addFilter(
-  'blocks.registerBlockType',
-  'x/blockFilters',
-  (settings, name) => {
-
-    // skip block types without wrapper or have known issues
-    const skip_blocks = [
-      'core/html',
-      'core/shortcode',
-      'core/block',
-      'core/file',
-    ];
-    if (skip_blocks.indexOf(name) !== -1) {
-      return settings;
-    }
-
-    return lodash.assign({}, settings, {
-      supports: lodash.assign({}, settings.supports, {
-        className: true
-      }),
-    });
-  }
-);
+// /**
+//  * Force default block classnames (ie. 'wp-block-list')
+//  */
+// wp.hooks.addFilter(
+//   'blocks.registerBlockType',
+//   'x/blockFilters',
+//   (settings, name) => {
+//
+//     // skip block types without wrapper or have known issues
+//     const skip_blocks = [
+//       'core/html',
+//       'core/shortcode',
+//       'core/block',
+//       'core/file',
+//     ];
+//     if (skip_blocks.indexOf(name) !== -1) {
+//       return settings;
+//     }
+//
+//     return lodash.assign({}, settings, {
+//       supports: lodash.assign({}, settings.supports, {
+//         className: true
+//       }),
+//     });
+//   }
+// );
--- wp-content/themes/x/dist/scripts/editor-gutenberg.js
+++ wp-content/themes/x/dist/scripts/editor-gutenberg.js
@@ -1,2 +1,2 @@
-"use strict";wp.hooks.addFilter("editor.PostFeaturedImage.imageSize","x/featured-image-size",(e,s,o)=>"medium"),wp.hooks.addFilter("blocks.registerBlockType","x/blockFilters",(e,s)=>{return-1!==["core/html","core/shortcode","core/block","core/file"].indexOf(s)?e:lodash.assign({},e,{supports:lodash.assign({},e.supports,{className:!0})})});
+"use strict";wp.hooks.addFilter("editor.PostFeaturedImage.imageSize","x/featured-image-size",(e,i,t)=>"medium");
 //# sourceMappingURL=editor-gutenberg.js.map
