7a244e785bea13154ca5895df1b1c37f084fa890
gaby <gaby@polarstork.com>
2024-05-30 02:57:20 +0300

    cleanup

---
+++ wp-content/themes/x/modules/inputs/Tokens.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace X_Modules\Inputs;
+
+use X_UI\Core\AbstractTokens;
+class Tokens extends AbstractTokens {
+  private static $instance = null;
+  private function __construct() {
+    parent::__construct([
+        "bgColor" => "mate-black-900",
+        "titleColor" => "yellow-gold-300",
+        "textColor" => "white-white-50",
+    ]);
+  }
+
+  public static function getInstance() {
+    if (!self::$instance) {
+      self::$instance = new Tokens();
+    }
+    return self::$instance;
+  }
+}
