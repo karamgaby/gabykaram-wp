b29673496b09b60cc00f3d22e03b8d659395ed8d
Gaby Karam <gaby@polarstork.com>
2024-06-01 03:42:19 +0300

    update bug in loading admin css in x-ui-library

--- wp-content/plugins/x-ui-library/inc/Core/RegisterAssets.php
+++ wp-content/plugins/x-ui-library/inc/Core/RegisterAssets.php
@@ -23,7 +23,7 @@ class  RegisterAssets {
         'x-ui-library-admin-css',
         trailingslashit(X_UI_LIBRARY_PLUGIN_DIR_URL)  . 'dist/styles/admin.css',
         [],
-        x_last_edited('css')
+        $this->x_last_edited('css')
       );
 
     });
@@ -64,4 +64,44 @@ class  RegisterAssets {
 
     });
   }
+
+
+  /**
+   * Retrieves the last edited timestamp for a given asset.
+   *
+   * This function retrieves the last edited timestamp for a given asset type, such
+   * as CSS or JavaScript. The timestamps are cached in a global variable for the
+   * current request. If there is no cached value, it attempts to load the timestamps
+   * from a JSON file and save them to the global variable.
+   *
+   * @param string $asset The asset type (default: 'css').
+   *
+   * @return int The last edited timestamp for the given asset. If the asset is not found
+   *             or the timestamps are not available, it returns 0.
+   */
+  private function x_last_edited($asset = 'css') {
+
+    global $x_timestamps;
+
+    // save timestamps to cache in global variable for this request
+    if (empty($x_timestamps)) {
+
+      $filepath = trailingslashit( X_UI_LIBRARY_PLUGIN_DIR_URL) . 'assets/last-edited.json';
+
+      if (file_exists($filepath)) {
+        $json = file_get_contents($filepath);
+        $x_timestamps = json_decode($json, true);
+      }
+
+    }
+
+    // use cached value from global variable
+    if (isset($x_timestamps[$asset])) {
+      return absint($x_timestamps[$asset]);
+    }
+
+    return 0;
+
+  }
+
 }
